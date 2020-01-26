269ce0584582e9bbf0bf48f66d1d3cfcb0b45767
Christine Bussinger <christinebussingerdev@gmail.com>
2020-01-25 20:39:59 -0500

    added search bar

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,10 +1,19 @@
 import React, { useState } from "react";
+import { Form, Field, withFormik} from 'formik';
 
-export default function SearchForm() {
- 
+const SearchForm = ({errors, touched, values, status}) => {
+  const [search, setSearch] = useState("")
+
   return (
     <section className="search-form">
-     // Add a search form here
+     <Form>
+       <Field type='text' name='currentSearch' placeholder='search for a character'/>
+       <button type='submit'>Submit</button>
+     </Form>
     </section>
   );
 }
+
+const FormikSearchForm = withFormik({})(SearchForm);
+
+export default FormikSearchForm;
--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import CharacterList from './CharacterList';
+import SearchForm from './SearchForm';
 
 export default function WelcomePage() {
   return (
@@ -12,7 +13,7 @@ export default function WelcomePage() {
           src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"
           alt="rick"
         />
-
+        <SearchForm />
         <CharacterList />
       </header>
     </section>
